fb4b3eaf151e8f3cf36e0dc4ea78b0e718b7478d
Jacob Marshall <29145479+jacobhq@users.noreply.github.com>
2021-07-06 20:06:47 +0100

    🍱 Move image again

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,7 +22,7 @@ function Hero () {
                     </div>
                 </div>
                 <div className="w-full mx-auto mt-20 text-center md:w-10/12">
-                    <img src="./assets/disc.png" alt="Hellonext feedback boards software screenshot" className="w-full rounded-lg" />
+                    <img src="./disc.png" alt="Hellonext feedback boards software screenshot" className="w-full rounded-lg" />
                     <div className="absolute overlay"></div>
                 </div>
             </section>
