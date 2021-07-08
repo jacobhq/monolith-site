97968f6b798d73ac6d639bba4f096c9b00db2317
jacobhq <jacob.marshall.jem@gmail.com>
2021-07-08 10:37:54 +0000

    ✨ Pausing

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,6 +11,14 @@ function Hero() {
         setPlaying(false)
     }
 
+    function Toggle() {
+        if (playing = true) {
+            Pause()
+        } else {
+            Play()
+        }
+    }
+
     return (
         <div className="pt-24">
             <section className="px-4 py-24 mx-auto max-w-7xl">
@@ -22,7 +30,7 @@ function Hero() {
                         Monolith is a podcast about all things tech with crazy antic and a huge personality there is no other podcast like this! (Hosted by Matthew Bhatti and Jacob Marshall)
                     </p>
                     <div className="mb-4 space-x-0 md:space-x-2 md:mb-8">
-                        <a className="inline-flex items-center justify-center w-full mb-2 btn btn-primary btn-lg sm:w-auto sm:mb-0 cursor-pointer" onClick={Play}>
+                        <a className="inline-flex items-center justify-center w-full mb-2 btn btn-primary btn-lg sm:w-auto sm:mb-0 cursor-pointer" onClick={Toggle}>
                             Play now
                             <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd" />
